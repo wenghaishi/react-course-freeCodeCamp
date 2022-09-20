7b5cd0251de1842f5e1c62edffb1db1ba757caf5
sam <wenghaishi1@gmail.com>
2022-09-20 17:04:27 +0800

    created simple-page

---
+++ simple-page/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from'./comps/Navbar';
+import './Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-page/src/comps/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <img className='nnav--ico' src="logo.svg" alt="" />
+        <h1 className="nav--logo_text ">React Facts</h1>
+        <h2 className='nav--title'>React Course Project-1</h2>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
