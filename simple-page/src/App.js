ac299ec8f9abbd0f67c76fdb20177fca8cd8bb38
sam <wenghaishi1@gmail.com>
2022-09-21 08:03:47 +0800

    completed siomple-page

--- simple-page/src/App.js
+++ simple-page/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from'./comps/Navbar';
+import Main from './comps/Main';
 import './Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Main />
     </div>
   );
 }
--- simple-page/src/comps/Main.js
+++ simple-page/src/comps/Main.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <main>
+      <div className='App-main'>
+        <h1>Fun facts about react</h1>
+        <ul className='Main-list'>
+          <li>Was released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well ovwe 100k stars on github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, incliding mobile apps</li>
+        </ul>
+      </div>
+    </main>
   )
 }
 
