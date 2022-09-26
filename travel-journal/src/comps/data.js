9dab26d2e86913e5b4799e3b5cbe5ee26227d815
sam <wenghaishi1@gmail.com>
2022-09-26 16:20:05 +0800

    minot fixes

--- travel-journal/src/comps/data.js
+++ travel-journal/src/comps/data.js
@@ -5,7 +5,7 @@ export default [
         location: "Mount Fuji",
         duration: "12 Jan, 2021 - 24 Jan, 2021",
         link: "https://www.google.com/maps/place/Mount+Fuji/@35.3606247,138.7186086,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Commonly called “Fuji-san, it is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
 
     },
     {
